ac7efeeedb1ac95c7a7e7e154835946072162f49
tom-foster <19tfoster87@gmail.com>
2017-05-01 22:10:47 +0100

    using flask bootstrap to extend using bootstrap

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, make_response, redirect, abort, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/redirect')
+def the_redirect_function():
+    '''Redirect example'''
+    return redirect('http://maps.warwickshire.gov.uk/ias')
+
+if __name__ == '__main__':
+    manager.run()
