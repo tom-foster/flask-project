9eb4fe19f77afea6103b5f19d440e3f943378704
tom-foster <19tfoster87@gmail.com>
2017-05-01 18:39:33 +0100

    setting a cookie in flask

--- python/hello.py
+++ python/hello.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    user_agent = request.headers.get('User-Agent')
+    return response
 
 @app.route('/user/<name>')
 def user(name):
