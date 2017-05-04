b15ae31718317a8dde545480e053c26d993bee22
tom-foster <19tfoster87@gmail.com>
2017-05-04 22:20:01 +0100

    implemented wtf form

--- hello.py
+++ hello.py
@@ -1,14 +1,25 @@
+from datetime import datetime
 from flask import Flask, request, make_response, redirect, abort, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+#a secret key should be set as an environmental variable
+#through etc/environment on linux - other os's are available
+app.config['SECRET_KEY'] = 'thispassworldshouldbeanenvironmentvariable'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -18,9 +29,15 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(),
+                           form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
