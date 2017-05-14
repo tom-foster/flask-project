6952860323aa35be579a02afb6f4210581270110
tom-foster <19tfoster87@gmail.com>
2017-05-14 22:59:24 +0100

    typo

--- hello.py
+++ hello.py
@@ -28,7 +28,7 @@ db = SQLAlchemy(app)
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name =  db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64), unique=True)
 
     def __repr__(self):
         return '<Role %r>' % self.name
