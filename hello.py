22283231f48114ca70e71d7209a0afec17257c4c
tom-foster <19tfoster87@gmail.com>
2017-05-29 13:44:35 +0100

    updated view to include adding a user to the db

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, request, make_response, redirect, abort, \
+from flask import Flask, request, make_response, redirect, \
      render_template, url_for, session, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -60,13 +60,18 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("I see you've changed name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', current_time=datetime.utcnow(),
-                           form=form, name=session.get('name'))
+                           form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
