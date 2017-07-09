2745a872bc68cff55fd9b69dc659a189ab8b34a5
tom-foster <19tfoster87@gmail.com>
2017-07-09 23:01:06 +0100

    I need to move forms next, and double check this function

---
+++ app/main/views.py
@@ -0,0 +1,33 @@
+#tf 09/07/17
+#now using blueprints
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASK_ADMIN']:
+                send_email(app.config['FLASK_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        ## Blueprints introduce the name space, so there are no conflicting errors
+        ## in larger apps.
+        ## you use .index to refer to main.index.
+        ## redirects across blueprints must use the name spaced endpoint name.
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           current_time=datetime.now())
