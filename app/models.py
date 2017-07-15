adb40d138a4679d06258b3d37e68f4e4090f0631
tom-foster <19tfoster87@gmail.com>
2017-07-15 23:19:15 +0100

    added user loaded call back function

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask_login import UserMixin
+from . import db, login_manager
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -37,3 +37,6 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
     
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
