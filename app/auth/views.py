171ee045219fd3f96dba206262ed6f8ffe125c45
tom-foster <19tfoster87@gmail.com>
2017-07-16 20:17:55 +0100

    altered the login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,21 @@
 #views for auth blueprint
 #14/07/17 tf
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_required, login_user, logout_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    ## templates are relative to the app's template folder
-    ## it's where Flask will search for the templates
-    ## you need to create a folder in templates called auth
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Your username or password is wrong!')
+    return render_template('auth/login.html', form=form)
 
 
 @auth.route('/secret')
