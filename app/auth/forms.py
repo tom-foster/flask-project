474f96f1915a9a83b25eda88e41ff851b5b3fe24
tom-foster <19tfoster87@gmail.com>
2017-07-16 20:37:14 +0100

    typo in loginform corrected

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import Required, Length, Email
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
                                              Email()])
-    password = PasswordField('Password', validators=[Requird()])
+    password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
