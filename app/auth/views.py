eb512646490d7e15f517dec33c6570bdc735096a
tom-foster <19tfoster87@gmail.com>
2017-07-23 23:07:10 +0100

    added a resend token to the views

--- app/auth/views.py
+++ app/auth/views.py
@@ -70,3 +70,12 @@ def confirm(token):
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm your account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email ahs been sent to you by email.')
+    return redirect(url_for('main.index'))
