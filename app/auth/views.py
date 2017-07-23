92753c1dfb0c7244e7bda27091622dda0e510601
tom-foster <19tfoster87@gmail.com>
2017-07-23 22:11:19 +0100

    Added confirm token to view, which is being used in the email/confirm

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 #views for auth blueprint
 #14/07/17 tf
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from . import auth
 from .. import db
 from ..models import User
@@ -43,3 +43,14 @@ def register():
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
 
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
