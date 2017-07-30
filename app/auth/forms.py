71e2c36fdf8689a7bd9eaeb2d6346f6200e1d2ad
tom-foster <19tfoster87@gmail.com>
2017-07-30 21:31:42 +0100

    Password Reset Request Form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -40,3 +40,9 @@ class ChangePasswordForm(FlaskForm):
         Required(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField('Confirm new password', validators=[Required()])
     submit = SubmitField('Update password')
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                            Email()])
+    submit = SubmitField('Reset Password')
+    
