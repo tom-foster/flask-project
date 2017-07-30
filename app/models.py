620fa18d69d15d6947e0c6cb7f8e822906cfa6c0
tom-foster <19tfoster87@gmail.com>
2017-07-30 22:53:50 +0100

    added reset password

--- app/models.py
+++ app/models.py
@@ -59,6 +59,19 @@ class User(UserMixin, db.Model):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset' : self.id})
 
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = self.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
