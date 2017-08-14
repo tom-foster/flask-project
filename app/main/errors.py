0484d49fafa71c02830aa1358929926664aa748e
tom-foster <19tfoster87@gmail.com>
2017-08-14 23:07:07 +0100

    added the 403 page so the app recognises it

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,6 +5,10 @@ from . import main
 
 #app_errorhandler makes the error application wide,
 #not just errors from the blueprint
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
