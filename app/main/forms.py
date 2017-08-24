9da3d7f46a805b24234042e88054d79d4c625146
tom-foster <19tfoster87@gmail.com>
2017-08-24 22:26:39 +0100

    started on the user level profile editor

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,18 @@
 #now using blue prints
 #tf 11/07/17
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, \
+SelectField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
