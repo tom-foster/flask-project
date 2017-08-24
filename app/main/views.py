325eb06c35cfd48c8e084154de305e4a68fd60de
tom-foster <19tfoster87@gmail.com>
2017-08-24 22:46:07 +0100

    added the view for edit-profile do the template next

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,13 @@
 #tf 09/07/17
 #now using blueprints
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app, abort
-from flask_login import login_required
+from flask import render_template, session, redirect, url_for, current_app,\
+abort, flash
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
-from ..models import User, Permission
+from ..models import User, Permission, Role
 from ..emails import send_email
 from ..decorators import admin_required, permission_required
 
@@ -54,3 +55,19 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
