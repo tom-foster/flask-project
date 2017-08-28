81552cbda328dec219d92f93bed128e749078002
tom-foster <19tfoster87@gmail.com>
2017-08-28 22:57:51 +0100

    made a change to gravatar method so if avatar_hash is available use that otherwise generate a new one

--- app/models.py
+++ app/models.py
@@ -166,7 +166,8 @@ class User(UserMixin, db.Model):
             url = 'https://secure.gravatar.com/avatar'
         else:
             url = 'http://www.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
