fb6235a4f8d24ff76a8a92800a12e88caecfbfce
tom-foster <19tfoster87@gmail.com>
2017-09-04 22:39:38 +0100

    added the pagination for user as well

--- app/main/views.py
+++ app/main/views.py
@@ -25,7 +25,8 @@ def index():
         page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts,
+                            pagination=pagination)
 
 # examples for the new decorators that have been made.
 @main.route('/admin')
@@ -46,8 +47,13 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts,
+                           pagination=pagination)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
