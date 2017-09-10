f7f5a8242800866679a1a5a83102dd68a7454abc
tom-foster <19tfoster87@gmail.com>
2017-09-10 22:04:38 +0100

    Added the edit post view

--- app/main/views.py
+++ app/main/views.py
@@ -102,3 +102,24 @@ def post(id):
     """Pass in a post as a list so that you can reuse _posts.html for post.html"""
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    """
+    This will use the same PostForm as defined in the model,
+    and used in the index
+    """
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
