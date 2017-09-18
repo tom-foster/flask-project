836a6c45f0885c5905b0e2b7a3000316f03a8f3b
tom-foster <19tfoster87@gmail.com>
2017-09-18 21:24:05 +0100

    the follows association table has been added as a model, this table relates the self referrential table users to itself by way of this association table

--- app/models.py
+++ app/models.py
@@ -48,6 +48,14 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
