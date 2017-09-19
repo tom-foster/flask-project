1e1e824f5a433af1195220bac2b80d1aea47fa1c
tom-foster <19tfoster87@gmail.com>
2017-09-19 16:43:19 +0100

    added a docstring for the join to make it clearer

--- app/models.py
+++ app/models.py
@@ -240,7 +240,7 @@ class User(UserMixin, db.Model):
 
     @property
     def followed_posts(self):
-        """ Obtain followed posts by a join. """
+        """ Obtain followed posts by a join on followed_id and author_id. """
         return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
             .filter(Follow.follower_id == self.id)
 
