dccd5872ae828c22c0dddb46b1047df35ae6730b
tom-foster <19tfoster87@gmail.com>
2017-09-19 15:20:44 +0100

    view for followers done

--- app/main/views.py
+++ app/main/views.py
@@ -153,3 +153,21 @@ def unfollow(username):
     current_user.unfollow(user)
     flash('You are no longer following %s' % username)
     return redirect(url_for('.user', username=username))
+
+@main.route('/followers/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def followers(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followers.paginate(
+        page, per_page=current_app.config['FLASK_FOLLOWERS_PER_PAGE'],
+        error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
+               for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followers of",
+                           endpoint='.followers', pagination=pagination,
+                           follows=follows)
