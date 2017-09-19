04ae44b0685061c1c50479b138aeef2b42a6646d
tom-foster <19tfoster87@gmail.com>
2017-09-19 22:02:42 +0100

    /followed view will show followed when someone returns if the link is clicked in the template tabs

--- app/main/views.py
+++ app/main/views.py
@@ -201,3 +201,9 @@ def show_all():
     resp.set_cookie('show_followed', '', max_age=30*24*60*60)
     return resp
 
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
