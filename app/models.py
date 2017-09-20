dd89b51d9216f0342e15d6f6bfa9c759e2dc90af
tom-foster <19tfoster87@gmail.com>
2017-09-20 22:18:15 +0100

    added the model for Comment

--- app/models.py
+++ app/models.py
@@ -313,3 +313,15 @@ class Post(db.Model):
             tags=allowed_tags, strip=True))
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    
