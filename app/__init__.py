37b5437be3e6fa8bb8d53fcf233f16ee25dba2fc
tom-foster <19tfoster87@gmail.com>
2017-09-23 23:22:02 +0100

    added the api to the main init of app

--- app/__init__.py
+++ app/__init__.py
@@ -41,4 +41,7 @@ def create_app(config_name):
     # as /auth/login
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
     return app
