13b86a4d46edc96c9a602b559f355ad02a7aa5c7
tom-foster <19tfoster87@gmail.com>
2017-09-24 23:27:16 +0100

    5005 error now deals with json response

--- app/main/errors.py
+++ app/main/errors.py
@@ -17,7 +17,7 @@ def forbidden(e):
 @main.app_errorhandler(404)
 def page_not_found(e):
     if request.accept_mimetypes.accept_json and \
-            not request.accept_mimetypes.accet_html:
+            not request.accept_mimetypes.accept_html:
         response = jsonify({'error': 'not found'})
         response.status_code = 404
         return response
@@ -25,4 +25,9 @@ def page_not_found(e):
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
     return render_template('500.html'), 500
