f37e4fa17e5cd4eee90092369e3ea017ac0365d0
tom-foster <19tfoster87@gmail.com>
2017-09-24 23:37:43 +0100

    unauthorised request error created

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -3,6 +3,11 @@ from app.exceptions import ValidationError
 from . import api
 
 def bad_request(message):
-    response = jsonify({'error' : 'bad request', 'message': message})
+    response = jsonify({'error': 'bad request', 'message': message})
     response.status_code = 400
+    return response
+
+def unauthorized(message):
+    response = jsonify({'error': 'unauthorized', 'message': message})
+    response.status_code = 401
     return response
