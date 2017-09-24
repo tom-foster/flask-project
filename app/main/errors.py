15c8974007326b39a9f87783903ed6bb3f0a6f86
tom-foster <19tfoster87@gmail.com>
2017-09-24 23:24:10 +0100

    preparing the main errors to deal with json responses if a json request hits a 403

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,12 +1,17 @@
 #tf 09/07/17
 #now using blueprints
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 #app_errorhandler makes the error application wide,
 #not just errors from the blueprint
 @main.app_errorhandler(403)
 def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
     return render_template('403.html'), 403
 
 @main.app_errorhandler(404)
