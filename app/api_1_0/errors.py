b3be24a86aab1ac673495446091b463282dced90
tom-foster <19tfoster87@gmail.com>
2017-09-24 23:38:36 +0100

    forbidden request error handling created in api

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -11,3 +11,8 @@ def unauthorized(message):
     response = jsonify({'error': 'unauthorized', 'message': message})
     response.status_code = 401
     return response
+
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
