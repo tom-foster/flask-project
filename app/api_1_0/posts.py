eca02ada2c34bd7138e19b487305bc46afa60dab
tom-foster <19tfoster87@gmail.com>
2017-09-25 20:56:41 +0100

    added edit_post to api using a PUT

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -2,7 +2,6 @@ from flask import jsonify, request, g, url_for, current_app
 from .. import db
 from ..models import Post, Permission
 from . import api
-## i need to write the decorator
 from .decorators import permission required
 from .errors import forbidden
 
@@ -26,3 +25,14 @@ def new_post():
     db.session.commit()
     return jsonify(post.to_json()), 201, \
         {'Location': url_for('api.get_post', id=post.id, _external=True)}
+
+@api.route('/posts/<int:id>', methods=['PUT'])
+@permission_required(Permission.WRITE_ARTICLES)
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    if g.current_user != post.author and \
+            not g.current_user.can(Permission.ADMINISTER):
+        return forbidden('Insufficient permissions')
+    post.body = request.json.get('body', post.body)
+    db.session.add(post)
+    return jsonify(post.to_json())
