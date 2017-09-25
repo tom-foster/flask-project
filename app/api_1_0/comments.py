27d0d2ebe1e7f8c3f7170bc0a471a3cdcc2338ae
tom-foster <19tfoster87@gmail.com>
2017-09-25 21:44:45 +0100

    new post comment added to api

--- app/api_1_0/comments.py
+++ app/api_1_0/comments.py
@@ -51,3 +51,17 @@ def get_post_comments(id):
         'next': next,
         'count': pagination.total
     })
+
+@api.route('posts/<int:id>/comments/', methods=['POST'])
+@permission_required(Permission.COMMENT)
+def new_post_comment(id):
+    post = Post.query.get_or_404(id)
+    comment = Comment.from_json(request.json)
+    comment.author = g.current_user
+    comment.post = post
+    db.session.add(comment)
+    db.session.commit()
+    return jsonify(comment.to_json()), 201, \
+        {'Location': url_for('api.get_comment', id=comment.id,
+                             _external=True)}
+                             
