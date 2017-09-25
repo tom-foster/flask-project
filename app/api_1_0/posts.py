52d93c68a16e3ea638bfd7a82798aba8433655c7
tom-foster <19tfoster87@gmail.com>
2017-09-25 20:38:32 +0100

    get_post route added

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -12,3 +12,7 @@ def get_posts():
     posts = Post.query.all()
     return jsonify({'posts' : [post.to_json() for post in posts]})
 
+@api.route('/posts/<int:id>')
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
