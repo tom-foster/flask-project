8896b1bb90bc81af478f4b65486336dcda8c3a84
tom-foster <19tfoster87@gmail.com>
2017-09-25 16:40:50 +0100

    added to_json for comment in the model

--- app/models.py
+++ app/models.py
@@ -383,4 +383,16 @@ class Comment(db.Model):
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
 
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'post': url_for('api.get_post', id=self.post_id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id,
+                              _external=True)
+        }
+        return json_comment
+
 db.event.listen(Comment.body, 'set', Comment.on_changed_body)
