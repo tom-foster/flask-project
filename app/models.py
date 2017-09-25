46eaf46744de398706d6d162b3c22767b9521e49
tom-foster <19tfoster87@gmail.com>
2017-09-25 15:20:59 +0100

    also added a staticmethod to verify auth token once the token is decoded

--- app/models.py
+++ app/models.py
@@ -264,6 +264,15 @@ class User(UserMixin, db.Model):
                        expires_in=expiration)
         return s.dumps({'id': self.id}).decode('ascii')
 
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
 
     def __repr__(self):
         return '<User %r>' % self.username
