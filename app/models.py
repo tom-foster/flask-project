38e2673a4869ac9b23d7a821d61f67f789e0d048
tom-foster <19tfoster87@gmail.com>
2017-09-25 16:28:19 +0100

    added to_json to posts, to provide a json representation of the post

--- app/models.py
+++ app/models.py
@@ -333,6 +333,21 @@ class Post(db.Model):
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
 
+    def to_json(self):
+        """The representation to JSON"""
+        json_post = {
+            'url': url_for('api.get_post', id=self.id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id,
+                              _external=True),
+            'comments': url_for('api.get_post_comments', id=self.id,
+                                _external=True),
+            'comment_count': self.comments.count()
+        }
+        return json_post
+
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 class Comment(db.Model):
