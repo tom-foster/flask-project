760ada8940ff33d244691d69548e23dc7d117f17
tom-foster <19tfoster87@gmail.com>
2017-09-25 16:09:45 +0100

    amended the verify_password on the api auth to take a token or email and password

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -7,14 +7,19 @@ from .errors import unauthorized, forbidden
 auth = HTTPBasicAuth()
 
 @auth.verify_password
-def verify_password(email, password):
-    if email == '':
+def verify_password(email_or_token, password):
+    if email_or_token == '':
         g.current_user = AnonymousUser()
         return True
-    user = User.query.filter_by(email = email).first()
+    if password == '':
+        g.current_user = User.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    user = User.query.filter_by(email=email_or_token).first()
     if not user:
         return False
     g.current_user = user
+    g.token_used = False
     return user.verify_password(password)
 
 @auth.error_handler
