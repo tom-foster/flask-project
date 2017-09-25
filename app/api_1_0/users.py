4d8cc68151c6b96b5dafbced8f8a8db8868876ea
tom-foster <19tfoster87@gmail.com>
2017-09-25 22:44:06 +0100

    get user followed posts, which makes use of the followed_posts property

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -28,3 +28,26 @@ def get_user_posts(id):
         'count': pagination.total
     })
 
+@api.route('/users/<int:id>/timeline/')
+def get_user_followed_posts(id):
+    user = User.query.get_or_404(id)
+    page = request.arg.get('page', 1, type=int)
+    ## use the property as followed_posts which is a sql query
+    pagination = user.followed_posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_user_followed_posts', page=page-1,
+                       _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_user_followed_posts', page=page+1,
+                       _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
