ffdf046ebd3c5687d874ee09e8f64b152e1097d3
tom-foster <19tfoster87@gmail.com>
2017-09-25 16:17:56 +0100

    added the api route to get token, and return it

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -36,3 +36,11 @@ def before_request():
     if not g.current_user.is_anonymous and \
             not g.current_user.confirmed:
         return forbidden('Unconfirmed account')
+
+@api.route('/token')
+def get_token():
+    if g.current_user.is_anonymous or g.token_used:
+        return unauthorized('Invalid credentials')
+    return jsonify({'token': g.current_user.generate_auth_token(
+        expiration=3600), 'expiration': 3600})
+        
