e91e6dae5228ed1965dcee547ec594dc36475593
tom-foster <19tfoster87@gmail.com>
2017-09-25 21:25:30 +0100

    amended get_posts to return paginated resource collections

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -8,8 +8,27 @@ from .errors import forbidden
 
 @api.route('/posts/')
 def get_posts():
-    posts = Post.query.all()
-    return jsonify({'posts' : [post.to_json() for post in posts]})
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate(
+        page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
+
+    # posts = Post.query.all()
+    # return jsonify({'posts' : [post.to_json() for post in posts]})
 
 @api.route('/posts/<int:id>')
 def get_post(id):
