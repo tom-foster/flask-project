29b90407c507122a600a667259fa9a515f8530b8
tom-foster <19tfoster87@gmail.com>
2017-09-25 21:40:11 +0100

    get post comments added

--- app/api_1_0/comments.py
+++ app/api_1_0/comments.py
@@ -28,3 +28,26 @@ def get_comments():
 def get_comment(id):
     comment = Comment.query.get_or_404(id)
     return jsonify({comment.to_json()})
+
+@api.route('/posts/<int:id>/comments/')
+def get_post_comments(id):
+    post = Post.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['FLASK_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_post_comments', id=id, page=page-1,
+                       _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_post_comments', id=id, page=page+1,
+                       _external=True)
+    return jsonify({
+        'comments': [comment.to_json() for comment in comments],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
