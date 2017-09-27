a30454bb348ac618a4bf925467de7682276db545
tom-foster <19tfoster87@gmail.com>
2017-09-27 14:47:58 +0100

    started writing the tests for the client side of the application

---
+++ tests/test_client.py
@@ -0,0 +1,12 @@
+import unittest
+from app import create_app, db
+from app.models import User, Role
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_role()
+        self.client = self.app.test_client(use_cookies=True)
