6ee35bf62c1565beaeb07a169e57b07af3579807
tom-foster <19tfoster87@gmail.com>
2017-09-27 16:04:05 +0100

    created an api test case, setUp and tearDown are done

---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+import json
+import unittest
+from base64 import b64encode
+from flask import url_for
+from app import create_app, db
+from app.models import User, Role, Post, Comment
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+        
