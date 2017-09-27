0f4e315f2331f0180e2334cd14e7ddce750d656c
tom-foster <19tfoster87@gmail.com>
2017-09-27 22:22:58 +0100

    started to add selenium test case, setup has been added

---
+++ tests/test_selenium.py
@@ -0,0 +1,50 @@
+import re
+import threading
+import time
+import unittest
+from selenium import webdriver
+from app import create_app, db
+from app.models import Role, User, Post
+
+class SeleniumTestCase(unittest.TestCase):
+    client = None
+    
+    @classmethod
+    def setUpClass(cls):
+        # start firefox
+        try:
+            cls.client = webdriver.Firefox()
+        except:
+            pass
+        
+        # skip these tests if the browser could not be started
+        if cls.client:
+            # create the application
+            cls.app = create_app('testing')
+            cls.app_context = cls.app.app_context()
+            cls.app_context.push()
+
+            #suppress logging to keep unittest output clean
+            import logging
+            logger = logging.getLogger('werkzeug')
+            logger.setLevel('ERROR')
+
+            #create the db and populate with some fake data
+            db.create_all()
+            Role.insert_roles()
+            User.generate_fake(10)
+            Post.generate_fake(10)
+
+            # add an admin user
+            admin_role = Role.query.filter_by(permission=0xff).first()
+            admin = User(email='tom@example.com',
+                         username='tom', password='hello',
+                         role=admin_role, confirmed=True)
+            db.session.add(admin)
+            db.session.commit()
+
+            # start the Flask server in a thread
+            threading.Thread(target=cls.app.run).start()
+
+            # give the server a second to ensure it is up
+            time.sleep(1)
