d383d332f0bc9301000a0911a64bfd01f9b873ab
tom-foster <19tfoster87@gmail.com>
2017-09-27 14:57:35 +0100

    added a doc string for homepage test - arguably might not be needed :P

--- tests/test_client.py
+++ tests/test_client.py
@@ -18,6 +18,10 @@ class FlaskClientTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_home_page(self):
+        """
+        Test if a user isn't logged in it returns the anonymous user greeting.
+        """
         response = self.client.get(url_for('main.index'))
         ## as text converts from byte array to unicode string
         self.assertTrue('anonymous user' in response.get_data(as_text=True))
+        
