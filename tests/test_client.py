7138d90d60746b77c1992a2ffa56656f69d11e45
tom-foster <19tfoster87@gmail.com>
2017-09-27 14:54:43 +0100

    test that the homepage returns anonymouse user, as no one is currently loggedi n

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import url_for
 from app import create_app, db
 from app.models import User, Role
 
@@ -8,11 +9,15 @@ class FlaskClientTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        Role.insert_role()
+        Role.insert_roles()
         self.client = self.app.test_client(use_cookies=True)
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-        
+
+    def test_home_page(self):
+        response = self.client.get(url_for('main.index'))
+        ## as text converts from byte array to unicode string
+        self.assertTrue('anonymous user' in response.get_data(as_text=True))
