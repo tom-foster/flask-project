f1aa89d3b9539a50968f50ccf2b03ac234a87caf
tom-foster <19tfoster87@gmail.com>
2017-09-27 15:24:28 +0100

    one test for registering, logging in with - 'need to confirm message', then confirmed, and finally logging out

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from flask import url_for
 from app import create_app, db
@@ -24,4 +25,40 @@ class FlaskClientTestCase(unittest.TestCase):
         response = self.client.get(url_for('main.index'))
         ## as text converts from byte array to unicode string
         self.assertTrue('anonymous user' in response.get_data(as_text=True))
-        
+
+    def test_register_and_login(self):
+        """
+        Test if a user registers for a new account, tries to login with new acccount
+        without confirming, then confirms account, then logouts.
+        """
+        # register a new account
+        response = self.client.post(url_for('auth.register'), data={
+            'email' : 'tom@example.com',
+            'username': 'userTom',
+            'password': 'hello',
+            'password2': 'hello'
+        })
+        self.assertTrue(response.status_code == 302)
+
+        # login with the new account and get greeted with not confirmed"
+        response = self.client.post(url_for('auth.login'), data={
+            'email' : 'tom@example.com',
+            'password' : 'hello',
+        }, follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertTrue(re.search('Hello,\s+userTom', data))
+        self.assertTrue('You have not confirmed your account yet.' in data)
+
+        # send a confirmation token
+        user = User.query.filter_by(email='tom@example.com').first()
+        token = user.generate_confirmation_token()
+        response = self.client.get(url_for('auth.confirm', token=token),
+                                   follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertTrue('You have confirmed your account. Thanks!' in data)
+
+        #log out
+        response = self.client.get(url_for('auth.logout'),
+                                   follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertTrue('You have now been signed out.' in data)
