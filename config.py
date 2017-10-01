660e26820e0b443b4885b04dfa7a46cb2e771c93
tom-foster <19tfoster87@gmail.com>
2017-10-01 21:14:20 +0100

    added heroku config for deployment to heroku it inherits from production but logs will stream logs as warning

--- config.py
+++ config.py
@@ -66,9 +66,22 @@ class ProductionConfig(Config):
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stdder
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
 config = {
     'development' : DevelopmentConfig,
     'testing' : TestingConfig,
     'production' : ProductionConfig,
+    'heroku': HerokuConfig,
     'default' : DevelopmentConfig
 }
